447edd75302331ec2939c2d7ac792f1a8504f760
Sebastian Bean <sebbean@gmail.com>
2016-12-16 15:14:21 -0800

    include jquery-ui in vendor

--- lib/herd_legacy_js.rb
+++ lib/herd_legacy_js.rb
@@ -3,7 +3,6 @@ require 'ember-rails'
 require 'ember_script-rails'
 require 'emblem/rails'
 require 'jquery-rails'
-require 'jquery-ui-rails'
 
 module HerdLegacyJs
   class Engine < ::Rails::Engine
